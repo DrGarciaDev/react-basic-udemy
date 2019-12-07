d659c5c85770cc47ea5ae84ce61211e6f829f3fa
Luis García <luis_grdz@hotmail.com>
2019-12-07 11:58:16 -0600

    Video 17: añadiendo react-bootstrap(bootstrap como componentes de react) y bootstrap 4.* al proyecto

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Button, Accordion, Card } from 'react-bootstrap';
 import Saludar from './components/Saludar';
 
 function App() {
@@ -12,6 +13,12 @@ function App() {
   return (
     <div className="App">
       <h1>Mi Primer Componente Recordando las bases</h1>
+      <br/><br/>
+      <Button variant="primary">Normal</Button>      
+      <br/><br/>
+      <Button variant="primary" size="lg">Large</Button>      
+      <br/><br/>
+      <Button variant="primary" onClick={() => console.log('Holas')}>OnClik</Button>      
       {/* {holaMundo()}
       <button onClick= {numero}>Click</button> */}
        <p>{/* Para que no se renderice una función al momento, sino enviar su lógica, se manda sin parentesís */}
@@ -20,6 +27,31 @@ function App() {
       <p>
         <Saludar nombre = "Izel" enviarSaludo={enviarSaludo}/>
       </p>
+      <br/><br/>
+      <hr/>
+      <h2>Uso de acordiones</h2>
+      <Accordion>
+        <Card>
+          <Card.Header>
+            <Accordion.Toggle as={Button} variant="link" eventKey="0">
+              Click me!
+            </Accordion.Toggle>
+          </Card.Header>
+          <Accordion.Collapse eventKey="0">
+            <Card.Body>Hello! I'm the body</Card.Body>
+          </Accordion.Collapse>
+        </Card>
+        <Card>
+          <Card.Header>
+            <Accordion.Toggle as={Button} variant="link" eventKey="1">
+              Click me!
+            </Accordion.Toggle>
+          </Card.Header>
+          <Accordion.Collapse eventKey="1">
+            <Card.Body>Hello! I'm another body</Card.Body>
+          </Accordion.Collapse>
+        </Card>
+      </Accordion>
     </div>
   );
 }
