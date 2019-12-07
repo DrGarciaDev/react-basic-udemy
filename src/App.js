f4867e7dfc25ef76442e0d81bb107fcafe9bf4c0
Luis García <luis_grdz@hotmail.com>
2019-12-07 14:13:29 -0600

    Video 21 y 22: instalando React router dom y creando un pequeño y básico sistema de rutas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import React from 'react';
-import Car from './components/Car';
+// import Car from './components/Car';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Contacto from './pages/Contacto';
+import QuienSoy from './pages/QuienSoy';
 
 function App() {
 
   return (
     <div className="App">
       <h1>Mi Primer Componente Recordando las bases</h1>
-      <Car/>
+      <h2>Route with React Router Dom</h2>
+      <Router>
+        <div>
+          <Link to="/">
+            <button>Home</button>
+          </Link>
+          <Link to="/contacto">
+            <button>Contacto</button>
+          </Link>
+          <Link to="/quien-soy">
+            <button>Quien Soy</button>
+          </Link>
+        </div>
+
+        <Switch>
+          <Route path="/contacto">
+            <Contacto/>
+          </Route>
+          <Route path="/quien-soy">
+            <QuienSoy/>
+          </Route>
+        </Switch>
+      </Router>
+      {/* <Car/> */}
     </div>
   );
 }
