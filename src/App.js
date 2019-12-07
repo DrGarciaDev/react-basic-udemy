0e950f438507566674adb1759385e8c221c54ee2
Luis García <luis_grdz@hotmail.com>
2019-12-07 09:59:20 -0600

    Primer componente, también muy básico pero de reafirmación de conocimientos

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
+import Saludar from './components/Saludar';
 
 function App() {
-  const holaMundo = () => {
-    return "Hola mundo";
-  }
+  // const holaMundo = () => {
+  //   return "Hola mundo";
+  // }
 
-  const numero = () => {
-    console.log('1');
-  }
+  // const numero = () => {
+  //   console.log('1');
+  // }
   return (
     <div className="App">
-      <h1>Mi Primer App Recordando las bases</h1>
-      {holaMundo()}
-      <button onClick= {numero}>Click</button>
+      <h1>Mi Primer Componente Recordando las bases</h1>
+      {/* {holaMundo()}
+      <button onClick= {numero}>Click</button> */}
+      <Saludar />
     </div>
   );
 }
---
+++ src/components/Saludar.js
@@ -0,0 +1,7 @@
+// import React from 'react';
+
+function Saludar () {
+    return 'Hola mundo versión - 2.0';
+}
+
+export default Saludar;
