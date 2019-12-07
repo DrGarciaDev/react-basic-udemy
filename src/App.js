b550831014b999f54afb0161cc1d1a03f7219915
Luis García <luis_grdz@hotmail.com>
2019-12-07 13:31:47 -0600

    Video 19: Usando los hooks de estado - useState

--- src/App.js
+++ src/App.js
@@ -1,62 +1,12 @@
 import React from 'react';
-import { Button, Accordion, Card } from 'react-bootstrap';
-import { ReactComponent as ReactIcon } from './assets/react.svg';
-import Saludar from './components/Saludar';
+import Car from './components/Car';
 
 function App() {
-  const enviarSaludo = (nombre) => {
-    console.log('Hola ' + nombre);
-  }
 
-  // const numero = () => {
-  //   console.log('1');
-  // }
   return (
     <div className="App">
       <h1>Mi Primer Componente Recordando las bases</h1>
-      <br/><br/>
-      <Button variant="primary">Normal</Button>      
-      <br/><br/>
-      <Button variant="primary" size="lg">Large</Button>      
-      <br/><br/>
-      <Button variant="primary" onClick={() => console.log('Holas')}>OnClik</Button>      
-      {/* {holaMundo()}
-      <button onClick= {numero}>Click</button> */}
-       <p>{/* Para que no se renderice una función al momento, sino enviar su lógica, se manda sin parentesís */}
-        <Saludar nombre = "Agustin" enviarSaludo={enviarSaludo}/>
-      </p>
-      <p>
-        <Saludar nombre = "Izel" enviarSaludo={enviarSaludo}/>
-      </p>
-      <br/>
-      <hr/>
-      {/* imagen como componente */}
-      <ReactIcon/>
-      <br/><br/>
-      <hr/>
-      <h2>Uso de acordiones</h2>
-      <Accordion>
-        <Card>
-          <Card.Header>
-            <Accordion.Toggle as={Button} variant="link" eventKey="0">
-              Click me!
-            </Accordion.Toggle>
-          </Card.Header>
-          <Accordion.Collapse eventKey="0">
-            <Card.Body>Hello! I'm the body</Card.Body>
-          </Accordion.Collapse>
-        </Card>
-        <Card>
-          <Card.Header>
-            <Accordion.Toggle as={Button} variant="link" eventKey="1">
-              Click me!
-            </Accordion.Toggle>
-          </Card.Header>
-          <Accordion.Collapse eventKey="1">
-            <Card.Body>Hello! I'm another body</Card.Body>
-          </Accordion.Collapse>
-        </Card>
-      </Accordion>
+      <Car/>
     </div>
   );
 }
---
+++ src/components/Car.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+
+function Car() {
+    const [started, setStarted] = useState(false);
+    const [countKm, setCountKm] = useState(0);
+
+    const checkStateCar = () => {
+        if (started) {
+            return <span style={{color: 'green'}}>Encendido</span>;
+        }
+        return <span style={{color: 'red'}}>Apagado</span>;
+    }
+
+    const increaseKm = (num) => {
+        if (started) {
+            setCountKm(countKm + num);
+        }
+        else{
+            alert('El coche está apagado');
+        }
+    }
+    return (
+        <div>
+            <h2>Nuestro carro está: {checkStateCar()}</h2>
+            <h2>Kilometros recorridos {countKm}</h2>
+            <button
+                onClick={() => setStarted(!started)}
+            >
+                Encender / Apagar
+            </button>
+            <button
+                onClick={() => {
+                    increaseKm(2)
+                }}
+            >
+                Incrementar Kilometros
+            </button>
+        </div>
+    );
+}
+
+export default Car;
